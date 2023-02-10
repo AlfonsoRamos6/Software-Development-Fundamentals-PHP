744e11736a6cf5064966a213ecb47d98551cd62c
alfonso <0257621@schoolsnet.act.edu.au>
2023-02-10 14:00:17 +1100

    index.php and edited template.php

--- template.php
+++ template.php
@@ -1,1 +1,12 @@
 <?php
+
+function footer(): string
+{
+    date_default_timezone_set('Australia/Canberra');
+    $filename = basename($_SERVER["SCRIPT_FILENAME"]);
+    $footer = "This page was last modified: " . date("F d Y H:i:s.", filemtime($filename));
+    return $footer;
+}
+
+
+?>

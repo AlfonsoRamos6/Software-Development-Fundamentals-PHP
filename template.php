04322f6f81df92c6ee46b0eeaddc0e2d174e1a85
alfonso <0257621@schoolsnet.act.edu.au>
2023-02-27 10:39:43 +1100

    added invoice and orderform to navbar

--- template.php
+++ template.php
@@ -21,6 +21,11 @@
                 <li class="nav-item">
                     <a class="nav-link" href="contact.php">Contact Us</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="orderForm.php">Order Form</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="invoice.php">Invoice</a>
                 </li>
             </ul>
         </div>

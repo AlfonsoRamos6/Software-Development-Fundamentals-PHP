68151ad8f14abf10098f49e9c3ba894c39ddf899
alfonso <0257621@schoolsnet.act.edu.au>
2023-06-07 11:57:01 +1000

    started analysis

--- login.php
+++ login.php
@@ -1,3 +1,17 @@
+<!--
+Analysis
+Once the user has entered a username and password in the username and password fields, the data will be sanitised
+Once the data has been entered, the username will be located (if the username exists) in the database
+If the count > 0, the emailaddress field has located the username.
+Once a user has been found, the data in the password field will be hashed, to check if it matches up with the username in the password field in the database
+If the password is seen to be correct, the user will see in the navbar a message saying "Login Successful", meaning the user hsa logged in successfully
+If either the username or password are not found in the database (case = 0), an error will be echoed saying "Invalid username or password"
+
+
+-->
+
+
+
 <?php
 /**  @var $conn */
 
